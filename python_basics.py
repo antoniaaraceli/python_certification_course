d8f34a4cbd153cd8efac6e7d3ba864fabefca71b
antoniaaraceli <antoniaaraceli.tech@gmail.com>
2026-02-25 11:47:31 -0600

    New notes about String manipulation

--- python_basics.py
+++ python_basics.py
@@ -57,5 +57,25 @@ sli_str = 'Hola Mundo!'
 ## there's also an optional step parameter, which is used to specify the increment between each index in the slice. string[start:stop:step]
 # print(sli_str[0:10:3]) # Hauo
 ## There's a helpful trick to reverse a string by setting step to -1, and leaving start and stop blank:
-print(sli_str[::-1]) # !odnuM aloH
+#print(sli_str[::-1]) # !odnuM aloH
+
+### String Methods
+str = 'Hola Mundo'
+# print(str1.upper()) # HOLA MUNDO
+# print(str.lower()) # hola mundo
+str2 = ' Hola Mundo '
+# print(str2.strip()) # Hola Mundo, without spaces
+# print(str.replace('Hola', 'Adiós')) # Adiós mundo
+# print(str.split()) # ['Hola', 'Mundo'] -> If no separator is specified, it splits on whitespace.
+
+my_gretting = ['Hola', 'Mundo']
+joined_str = ' '.join(my_gretting)
+#print(joined_str) # Hola Mundo
+#print(str.startswith('H')) # True
+#print(str.endswith('x')) # False
+#print(str.find('Mundo')) # 5 -> Returns the index of the first occurrence of substring, or -1 if it doesn't find one.
+#print(str.count('o')) # 2 -> Returns the number of times a substring appears in a string.
+#print(str.lower().capitalize()) # Hola mundo
+#print(str.lower().title()) # Hola Mundo
+
 
